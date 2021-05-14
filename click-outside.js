633a678c323968df2e33b7ef67ce70ea8bf16e7e
Gabe Valdivia <gabe@gabeswebcoding.com>
2021-05-14 01:07:28 -0500

    Add EventListener to buttons that listens for clicks

--- click-outside.js
+++ click-outside.js
@@ -1,1 +1,9 @@
-console.log('I am working!');
+const cardButtons = document.querySelectorAll('.card button');
+
+function handleButtonClick() {
+  console.log('You clicked a button!');
+}
+
+cardButtons.forEach((button) =>
+  button.addEventListener('click', handleButtonClick)
+);
