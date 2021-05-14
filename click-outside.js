25bb754e318a7e57a18dc5bd58f23bd35754578c
Gabe Valdivia <gabe@gabeswebcoding.com>
2021-05-14 01:21:15 -0500

    Write JS code to grab image source, description, title and populate the modal with the info

--- click-outside.js
+++ click-outside.js
@@ -1,9 +1,21 @@
 const cardButtons = document.querySelectorAll('.card button');
+const modalInner = document.querySelector('.modal-inner');
 
 function handleButtonClick(event) {
   const button = event.currentTarget;
   const card = button.closest('.card');
-  console.log(card);
+  // Grab the image src
+  const imgSrc = card.querySelector('img').src;
+  // Grab description
+  const desc = card.dataset.description;
+  // Grab Title
+  const name = card.querySelector('h2').textContent;
+  // Populate modal with the new info
+  modalInner.innerHTML = `
+    <img src="${imgSrc.replace('200', '600')}" alt="${name}"/>
+    <p>${desc}</p>
+  `;
+  // Show the modal
 }
 
 cardButtons.forEach((button) =>
